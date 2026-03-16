8d9f40122f0217a331ef8496dbda95d59f386da5
sinjukie <kalpenosjames@gmail.com>
2026-03-16 18:46:01 +1100

    Add the top level connection for the nios processor

--- top_level.v
+++ top_level.v
@@ -2,20 +2,23 @@ module top_level (
 	/* Add your inputs here */
 	
 	// Clock
+	input CLOCK50,
 		
 	// Keys
+	input [0:0]KEY,
 		
 	// Leds
+	output [9:0]LEDR,
 	
 	// Switches
-	
+	input [9:0]SW
 );
 
     nios_system u0 (
-        .clk_clk       (/* Connect the clock */),       
-        .reset_reset_n (/* Connect a Key for reset */), 
-        .ledr_export   (/* Connect all LEDs */),   
-        .sw_export     (/* Connect all Switches */)      
+        .clk_clk       (CLOCK50),       
+        .reset_reset_n (KEY), 
+        .ledr_export   (LEDR),   
+        .sw_export     (SW)     
     );
 
 	 
